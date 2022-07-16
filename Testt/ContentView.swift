1f877716c944f77ae77878266ebb97c59b8bae5a
Tiago Almeida <soares343vio@hotmail.com>
2022-07-16 20:23:40 +0100

    Initial Commit

---
+++ Testt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testt
+//
+//  Created by Tiago Xavier da Cunha Almeida on 16/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testt/TesttApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesttApp.swift
+//  Testt
+//
+//  Created by Tiago Xavier da Cunha Almeida on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TesttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
